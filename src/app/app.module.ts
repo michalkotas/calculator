d530cde7344f6cef0d4e16ff5248268f37a86a01
Kaiyue Xu <kaiyuexu09@gmail.com>
2021-11-23 09:52:18 +0100

    Add Ionic Components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,11 @@
-import { NgModule } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
+
 
 @NgModule({
   declarations: [
@@ -11,10 +13,14 @@ import { HomeComponent } from './components/home/home.component';
     HomeComponent
   ],
   imports: [
+    IonicModule,
     BrowserModule,
+    IonicRouteStrategy,
     AppRoutingModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  entryComponents: [],
+  bootstrap: [AppComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class AppModule { }
--- src/app/calculator/calculator.module.ts
+++ src/app/calculator/calculator.module.ts
@@ -1,12 +1,22 @@
-import { NgModule } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
 import { CalculatorRoutingModule } from './calculator-routing.module';
 import { HomeComponent } from './components/home/home.component';
 import { BridgeModule } from 'bridge';
+import { IonicModule} from '@ionic/angular';
+import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+
 
 @NgModule({
   declarations: [HomeComponent],
-  imports: [CommonModule, CalculatorRoutingModule, BridgeModule],
+  imports: [CommonModule, CalculatorRoutingModule, BridgeModule,IonicModule,FormsModule,
+    RouterModule.forChild([
+      {
+        path: '',
+        component: HomeComponent
+      }
+    ])],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class CalculatorModule {}
